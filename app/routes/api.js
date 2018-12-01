a7b8e341c13e3be1b120996331425d22514a723f
Sam Belfatto <sambelfatto@01875-MBP15.local>
2018-11-30 16:52:54 -0800

    add post endpoint

--- app/routes/api.js
+++ app/routes/api.js
@@ -6,10 +6,24 @@ module.exports = function (app, express) {
  // index route for the whole application
  app.get('/api/text', async function (req, res) {
   // display all of the texts in database
-
+  var results = await db.api.findAll()
+  res.json(results)
  })
  app.get('/api/text/:id', async function (req, res) {
   // get the text based on an ID in the database
-
+  var textId = req.params.id 
+  var queryResult = await db.api.findOne({
+      where: {
+          id: textId
+      }
+  })
+  res.json(queryResult)
  })
+app.post('/api/text', async function (req, res) {
+    var text = req.body.text
+    var queryResult = await db.api.create({
+        text: text
+    })
+res.json(queryResult)
+})
 }
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ require('dotenv').config()
 // Express config 
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 100000}))
 
 // import the database models 
 var models = require('./app/models') 
